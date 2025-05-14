56f8e6099edfdffd2cb76fe57f8ceea12308fac3
장문선 <118340567+munwalk@users.noreply.github.com>
2025-05-15 04:14:58 +0900

    Update store.service.js

--- src/services/store.service.js
+++ src/services/store.service.js
@@ -1,6 +1,8 @@
 import { createStore, getAllStoreReviews } from "../repositories/store.repository.js";
 import { StoreInputError } from "../errors.js";
 import { responseFromReviews } from "../dtos/review.dto.js";
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
 export const addStore = async ({ regionName, storeName }) => {
   if (!regionName || !storeName) {
@@ -16,4 +18,16 @@ export const addStore = async ({ regionName, storeName }) => {
 export const listStoreReviews = async (storeId) => {
   const reviews = await getAllStoreReviews(storeId);
   return responseFromReviews(reviews);
+};
+
+
+export const getStoreById = async (storeId) => {
+  return await prisma.store.findUnique({
+    where: { id: storeId },
+    select: {
+      id: true,
+      name: true,
+      regionName: true,
+    }
+  });
 };
