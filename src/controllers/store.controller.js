7e24e023aa17ea18394acefedc08a21f1438711b
장문선 <118340567+munwalk@users.noreply.github.com>
2025-05-15 04:14:24 +0900

    Update store.controller.js

--- src/controllers/store.controller.js
+++ src/controllers/store.controller.js
@@ -1,6 +1,8 @@
 import { StatusCodes } from "http-status-codes";
 import { addStore, listStoreReviews } from "../services/store.service.js";
 import { bodyToStore } from "../dtos/store.dto.js";
+import { getStoreById } from "../services/store.service.js";
+import { StoreNotFoundError } from "../errors.js";
 
 export const handleCreateStore = async (req, res, next) => {
   try {
@@ -16,6 +18,19 @@ export const handleListStoreReviews = async (req, res, next) => {
     const storeId = parseInt(req.params.storeId);
     const reviews = await listStoreReviews(storeId);
     res.status(StatusCodes.OK).success(reviews);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const handleGetStore = async (req, res, next) => {
+  try {
+    const storeId = parseInt(req.params.storeId);
+    const store = await getStoreById(storeId);
+    if (!store) {
+      throw new StoreNotFoundError("해당 ID의 가게가 존재하지 않습니다.", { storeId });
+    }
+    res.status(StatusCodes.OK).success(store);
   } catch (err) {
     next(err);
   }
